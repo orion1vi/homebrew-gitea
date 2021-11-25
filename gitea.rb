b62d43c15f2c06802d9a0f7c621d2b49a1ee58c4
Vorlif <vorlif@noreply.gitea.io>
2021-11-25 23:46:59 +0800

    Gitea 1.15.6 (#148)
    Update to 1.15.6
    
    Reviewed-on: https://gitea.com/gitea/homebrew-gitea/pulls/148
    Reviewed-by: techknowlogick <techknowlogick@gitea.io>
    Reviewed-by: Andrew Thornton <art27@cantab.net>
    Co-authored-by: Vorlif <vorlif@noreply.gitea.io>
    Co-committed-by: Vorlif <vorlif@noreply.gitea.io>

--- gitea.rb
+++ gitea.rb
@@ -1,7 +1,7 @@
 class Gitea < Formula
   desc "Git with a cup of tea, painless self-hosted git service"
   homepage "https://gitea.io"
-  version "1.15.4"
+  version "1.15.6"
 
   os = OS.mac? ? "darwin-10.12" : "linux"
   arch = case Hardware::CPU.arch
@@ -23,11 +23,11 @@ class Gitea < Formula
   end
 
   @@sha256 = case "#{os}-#{arch}"
-             when "linux-386" then "cb8d0d82ebea546d4f6db1aa80f62d5a382c9b1c3116278dff41e462352a0e5c"   # binary
-             when "linux-amd64" then "d44dc105a0d8dedf051e36b0eef7c94b56e0c478a78c3575ab1cacf2f4cf77c1"
-             when "linux-arm64" then "a40c22504507915d3acccc146052415b1ece5c86db2facf34d78fc887128515f" # binary
-             when "darwin-10.12-amd64" then "c2208e84a73536a881372ee301dc5872015af51dd9a31039b495010a656b5fca"
-             when "darwin-10.12-arm64" then "3efd6e61bb45c1fb9da0c5ecf84f05c15fd8a94caf2db2aefa5962a3270d49d7"
+             when "linux-386" then "b2047a887aa30b0ca432b97dd10085188b2717f91cdb59e24b1494eb4e1d0ecc"   # binary
+             when "linux-amd64" then "0733196e4f8e49548383b890845f08f901b9959afd79e2b3d493b200aa75b109"
+             when "linux-arm64" then "42fb32b7fd3c00cb32fc3c461d2d1cc6f8792f778a3b41de9e23adcb4c1dd1f1" # binary
+             when "darwin-10.12-amd64" then "84e505cc314a14b51c6133980df3e23125e271de9a4c1d5f04c67fc9f870721a"
+             when "darwin-10.12-arm64" then "9bcfc264e7049813267eb1f68ee7291c3bc687d12cbe95bcc9e64258fa97dff5"
              else
                raise "gitea: Unsupported system #{os}-#{arch}"
              end
