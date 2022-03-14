4b7d4fe4fbfcf1c678f1e945e622e7fd62efb5f8
drone <techknowlogick@gitea.io>
2022-03-14 22:14:53 +0000

    [skip ci] Updated Version via cron

--- gitea.rb
+++ gitea.rb
@@ -3,7 +3,7 @@ require_relative './utils/macos_codesign.rb'
 class Gitea < Formula
   desc "Git with a cup of tea, painless self-hosted git service"
   homepage "https://gitea.io"
-  version "1.16.3"
+  version "1.16.4"
 
   os = OS.mac? ? "darwin-10.12" : "linux"
   arch = case Hardware::CPU.arch
@@ -25,11 +25,11 @@ class Gitea < Formula
   end
 
   @@sha256 = case "#{os}-#{arch}"
-             when "linux-386" then "9a643ffa5ac38e81bdc49fdd8016229e6ca2b9387e1d2d767f5d4a7baded0592"   # binary
-             when "linux-amd64" then "d684950b757d90fc8c4d16d530ee30714524b6a24dd7a17558209b2de1d7672e"
-             when "linux-arm64" then "0d527eca60f8b5151ed626c76e0665c985f8644b84e0a4beeac5d79e44a49cfb" # binary
-             when "darwin-10.12-amd64" then "a793ad486832e7655f9576fd9f9db77ac8d6d9e8dd2bd5534d23924db2c786f8"
-             when "darwin-10.12-arm64" then "3e4d5d73dad20aec3785d8391867420dda2bdf461bd444859e369bc46f746332"
+             when "linux-386" then "016e4c7c99337edf4a44231e872aea4f29575a63efa9b15b39e3319efe962139"   # binary
+             when "linux-amd64" then "30d18aac7e00e2011a6369f6bf87c5a8cf59e9b8e594ed0761822c1b670b7a6c"
+             when "linux-arm64" then "570e334a327f4dc1b8412bf0af6423efc52508300ef54a646d63d4838dba0273" # binary
+             when "darwin-10.12-amd64" then "eec128f44c184098a454838b48c853879a5d7566d2b1a3dc8055a24ab234b950"
+             when "darwin-10.12-arm64" then "21337dfa514c0dfb09048515c201809eafa0f06b224354d40f980721f2240ac4"
              else
                raise "gitea: Unsupported system #{os}-#{arch}"
              end
