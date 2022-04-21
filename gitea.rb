e2c3b6d94dfb8dd5f9456463aaaab5ca64d0291a
drone <techknowlogick@gitea.io>
2022-04-21 01:08:18 +0000

    [skip ci] Updated Version via cron

--- gitea.rb
+++ gitea.rb
@@ -25,11 +25,11 @@ class Gitea < Formula
   end
 
   @@sha256 = case "#{os}-#{arch}"
-             when "linux-386" then "<?xml"   # binary
-             when "linux-amd64" then "<?xml"
-             when "linux-arm64" then "<?xml" # binary
-             when "darwin-10.12-amd64" then "<?xml"
-             when "darwin-10.12-arm64" then "<?xml"
+             when "linux-386" then "5e01b620dc9d0b903b9b57b5b1f9da41bad93fb14d0065b0715bf16c61b425c0"   # binary
+             when "linux-amd64" then "a5e4cdc21983ffa78647a08365d8016fe178476f6f49a251377003200d8768f4"
+             when "linux-arm64" then "d36e23b6c065c63d6dc6b8bfdd3a13ef02eea81fee17a16f623e049273e8ab8c" # binary
+             when "darwin-10.12-amd64" then "1fe1fbbe5e89a59493e8e618f58185fec5880ae99bef8d7a6289299e0c0a85c6"
+             when "darwin-10.12-arm64" then "c156975fefb6dd3ef3320208310216eea123ed5c89a39e810665925e240ab2c4"
              else
                raise "gitea: Unsupported system #{os}-#{arch}"
              end
