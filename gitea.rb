a7015cf49ba63ab047b0c64f3551f1ddc09488cc
techknowlogick <techknowlogick@gitea.io>
2023-03-14 03:19:47 -0400

    bump

--- gitea.rb
+++ gitea.rb
@@ -3,7 +3,7 @@ require_relative './utils/macos_codesign.rb'
 class Gitea < Formula
   desc "Git with a cup of tea, painless self-hosted git service"
   homepage "https://gitea.io"
-  version "1.18.3"
+  version "1.18.5"
 
   os = OS.mac? ? "darwin-10.12" : "linux"
   arch = case Hardware::CPU.arch
@@ -25,11 +25,11 @@ class Gitea < Formula
   end
 
   @@sha256 = case "#{os}-#{arch}"
-             when "linux-386" then "a11491fbbda899af4f54fc3ba22e6ea172633c6097223c6f83b42711021bcb5f"   # binary
-             when "linux-amd64" then "60d0f6e16ea7831a5f3acc7edf05ca7832403c8c53fd27876c4dbcc4059e1cd2"
-             when "linux-arm64" then "e22ccdd9c34aa8a272baa3b864d933a39d3496a78d040417f412b2cab0b3fb63" # binary
-             when "darwin-10.12-amd64" then "d23df79b81249d87dc34b92830b10916af26edc7145c8400964f41ea3b3c1c9a"
-             when "darwin-10.12-arm64" then "4f8034c2d6f2af8f2b59046470390cfd0c6cd48a63c6a01a7903f01e755b1ee9"
+             when "linux-386" then "0fab5fafa55370593ccaf1be2833cff95834ea006502112189973d229893b50c"   # binary
+             when "linux-amd64" then "60e9ecabde0f6f0f2bd943cf03db5d5dd4457608d4e8627e063e293397ad30b1"
+             when "linux-arm64" then "0792eec1005588d7ddb3fa0263bfd0dd90aeef1ba9ef9a93be6a3e763328fbaf" # binary
+             when "darwin-10.12-amd64" then "a8d978f748c7eba79a94c769aa2b4b7dba0af9db9922e30e092a2283b08c594f"
+             when "darwin-10.12-arm64" then "ef4ef7d799ead8a58672bbdbe85d4535c9f761510596178c95cb8c8f8ab66a8a"
              else
                raise "gitea: Unsupported system #{os}-#{arch}"
              end
--- tea.rb
+++ tea.rb
@@ -3,7 +3,7 @@ require_relative './utils/macos_codesign.rb'
 class Tea < Formula
   desc "A command line tool to interact with Gitea servers"
   homepage "https://gitea.com/gitea/tea"
-  version "0.9.0"
+  version "0.9.2"
 
   os = OS.mac? ? "darwin" : "linux"
   arch = case Hardware::CPU.arch
@@ -25,11 +25,11 @@ class Tea < Formula
   end
 
   @@sha256 = case "#{os}-#{arch}"
-             when "linux-386" then "8e5b0c068b43022c0ec678f4d2b565ba31a514e1821063db9db482ef4fb603c4"   # binary
-             when "linux-amd64" then "6ec8b5eb6a73e30bc1ba976bc7c4f6d74aeceea646ac8e318db262ea31f2ecb7"
-             when "linux-arm64" then "27394b535ad99937b930b28b56735ad9dbe5ec892df5bcb8ea9ea87e5634e151" # binary
-             when "darwin-amd64" then "83aa0f21821278b11d9dd01ae416ebf3f3cb618e6f56a9a3d53ea9d79027d04a"
-             when "darwin-arm64" then "25331254f489dbf47d087e0810daff63638abf178a212e85547ca571803c088e"
+             when "linux-386" then "f0d0080db67403bb872a3ff71d77f56f2fc38d95d1e72796e5e85fce4f2a7692"   # binary
+             when "linux-amd64" then "32ed8217e7facc718f45d272d8549ad3d2f82735855cc25bfca525c8f72a4d8d"
+             when "linux-arm64" then "be1aa529ca50dfad520206c21ee9c8b179482c11c4f462ddcfa0101d798fbaea" # binary
+             when "darwin-amd64" then "96b5696bc1fa3b65292ac3ac36809e1466df574c3537bd6b0d905ebadeae67bd"
+             when "darwin-arm64" then "fb5e295f901837aa6290942bad5cfd7f12fd218fbce8501e0f3f63807faae236"
              else
                raise "tea: Unsupported system #{os}-#{arch}"
              end
